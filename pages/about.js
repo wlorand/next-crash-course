9693ae081dabb157371278ca285ddc718c1eeb47
Will Lorand <will@milemarker3.org>
2022-02-11 14:30:28 -0800

    added head, about page, _app.js and initial layout

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import React from 'react';
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>All about it</title>
+      </Head>
+      <h1>About Time</h1>
+    </div>
+  );
+};
+
+export default about;
